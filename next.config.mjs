6138368f5223e966300e1630e81b76aedcf3a16f
Rashid Raza <rashidansari.gsb11@gmail.com>
2026-04-18 15:25:20 +0530

    added configure file

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // ✅ static export
-
-  basePath: '/chatbox/backoffice', // ✅ subfolder path
-  assetPrefix: '/chatbox/backoffice/',
-
-  images: {
-    unoptimized: true, // ✅ required for static export
-  },
-
   devIndicators: {
     appIsrStatus: false,
     buildActivity: false,
